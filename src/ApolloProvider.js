f9001bdac848506f003cb09ba938df1bd6669fce
Abayomi Shosilva <ashosilva@gmail.com>
2021-01-22 03:11:10 -0500

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://infinite-woodland-94473.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
